9f8ac6a5ed5a836792b76c463b79f74ceedc6a3b
Meris Čulić <meris.culic@gmail.com>
2020-02-28 13:53:57 +0100

    Add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,22 +16,60 @@ def client():
     yield client
 
 
+def login(client):
+    return client.post('/login', data={
+        "user-name": "Test User", "user-email": "test@user.com",
+        "user-password": "password123"},
+        follow_redirects=True
+    )
+
+
+def test_login(client):
+    login(client)
+    response = client.get("/")
+    assert b'Enter your guess' in response.data
+
+
 def test_index_not_logged_in(client):
     response = client.get('/')
     assert b'Enter your name' in response.data
 
 
-def test_index_logged_in(client):
-    client.post(
-        '/login', data={
-            "user-name": "Test User", "user-email": "test@user.com",
-            "user-password": "password123"},
-            follow_redirects=True
-    )
+def test_profile(client):
+    login(client)
+    response = client.get('/profile')
+    assert b'Your name:' in response.data
 
-    response = client.get('/')
-    assert b'Enter your guess' in response.data
+def test_profile_delete(client):
+    login(client)
+    response = client.get('/profile/delete')
+    assert b'Delete your profile' in response.data
 
+    response = client.post('/profile/delete', follow_redirects=True)
+    assert b'Enter your name' in response.data
+
+
+def test_profile_edit(client):
+    login(client)
+    response = client.get('/profile/edit')
+    assert response.status_code == 200
+    assert b'Edit your profile' in response.data
+
+    response = client.post('/profile/edit', data=
+                      {"profile-name": "Test User 2",
+                       "profile-email": "test2@user.com"},
+                       follow_redirects=True)
+
+    assert b'Test User 2' in response.data
+    assert b'test2@user.com' in response.data
+
+def test_all_users(client):
+    response = client.get('/users')
+    assert b'Test User' not in response.data
+
+    login(client)
+    response = client.get('/users')
+    assert b'Test User' in response.data
 
 def cleanup():
     db.drop_all()
